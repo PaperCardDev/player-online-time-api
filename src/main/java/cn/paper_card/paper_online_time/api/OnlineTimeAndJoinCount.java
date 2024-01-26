d0f02f50181522381976821558a9b8ef776c91bb
Paper99 <1398872296@qq.com>
2024-01-26 23:01:06 +0800

    1.0

---
+++ src/main/java/cn/paper_card/paper_online_time/api/OnlineTimeAndJoinCount.java
@@ -0,0 +1,7 @@
+package cn.paper_card.paper_online_time.api;
+
+public record OnlineTimeAndJoinCount(
+        long onlineTime,
+        long jointCount
+) {
+}
---
+++ src/main/java/cn/paper_card/paper_online_time/api/PlayerOnlineTimeApi.java
@@ -0,0 +1,20 @@
+package cn.paper_card.paper_online_time.api;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+@SuppressWarnings("unused")
+public interface PlayerOnlineTimeApi {
+
+    // 查询玩家的总计在线时长和进入次数
+    @NotNull OnlineTimeAndJoinCount queryTotalOnlineAndJoinCount(@NotNull UUID uuid) throws Exception;
+
+    @NotNull OnlineTimeAndJoinCount queryTimeAndCountBetween(@NotNull UUID uuid, long begin, long end) throws Exception;
+
+    // 增加玩家今天在线的时间
+    boolean addOnlineTimeToday(@NotNull UUID player, long cur, long online) throws Exception;
+
+    // 增加玩家的进入次数，加一
+    boolean addJoinCountToday(@NotNull UUID player, long cur) throws Exception;
+}
